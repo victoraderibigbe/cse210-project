06944d1e75d34e48f61acfc33fd9807ddd42dc08
Victor Aderibigbe <aderibigbetoluwani5@gmail.com>
2024-09-17 00:04:28 +0100

    Complete prep assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        double grade = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine();
+        string letter = "";
+        string sign = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+            if (grade % 10 < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+            if (grade % 10 >= 7)
+            {
+                sign = "+";
+            }
+            else if (grade % 10 < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+            if (grade % 10 >= 7)
+            {
+                sign = "+";
+            }
+            else if (grade % 10 < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+            if (grade % 10 >= 7)
+            {
+                sign = "+";
+            }
+            else if (grade % 10 < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}{sign}");
+
+        Console.WriteLine();
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You have an exceptional grade");
+        }
+        else
+        {
+            Console.WriteLine("Keep working hard, you can do better next time!");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Hello World! Welcome to Guess-Magic-Number Game");
+        Console.WriteLine();
+
+        string response = "";
+
+        do
+        {
+
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int userGuess;
+            int guessCount = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                userGuess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Guess Lower!");
+                }
+                else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Guess Higher!");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You guessed correctly.");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                    Console.WriteLine();
+                    Console.WriteLine("Do you want to continue? (yes/no) ");
+                    response = Console.ReadLine();
+                }
+
+            } while (userGuess != magicNumber);
+        } while (response == "yes");
     }
 }
