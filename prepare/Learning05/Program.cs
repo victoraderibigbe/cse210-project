6204bbe603e18f03e28ebb000205dfb69a37231a
Victor Aderibigbe <aderibigbetoluwani5@gmail.com>
2024-10-17 18:05:27 +0100

    Complete w05 learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment a1 = new Assignment("Victor Aderibigbe", "The Evolution of Modern Technology Over the Years");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment a2 = new MathAssignment("John Victory", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeWorkList());
+        Console.WriteLine();
+
+        WritingAssignment a3 = new WritingAssignment("Mary Rose", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
